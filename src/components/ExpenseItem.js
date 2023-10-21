14cf5d2a99f79890f09d9ed316a8da2b73b9d982
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-10-21 11:42:42 +0530

    more readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './expenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = ({expense}) => {
+
   return (
     <>
       
       <div className='expense-item'>
-      <div>{expense.date.toISOString()}</div>
+        <ExpenseDate date={expense.date}></ExpenseDate>
+      
       <div className='expense-item__description'>
         <h2>{expense.title}</h2>
         <div className='expense-item__price'>${expense.amount}</div>
