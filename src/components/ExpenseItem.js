ddb45ce0226c066e87ea36333e72411c150b2c9a
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-10-24 11:14:13 +0530

    useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './expenseItem.css'
 
 import ExpenseDate from './ExpenseDate'
 import Card from './Card'
 
 const ExpenseItem = ({expense}) => {
-  const handleDelete = () => {
-    
-    console.log('deleted')
+  const [amount, setAmount] = useState(expense.amount)
+  const handleUpdate = () => {
+    setAmount( 50)
+
+    console.log(amount)
   }
 
   return (
@@ -18,9 +20,9 @@ const ExpenseItem = ({expense}) => {
       
       <div className='expense-item__description'>
         <h2>{expense.title}</h2>
-        <div className='expense-item__price'>${expense.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-      <button onClick={handleDelete}>Delete</button>
+      <button onClick={handleUpdate}>Update</button>
     </div>
       
     </>
